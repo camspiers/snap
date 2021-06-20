76b136f05081aadbc9163de06ecf4c9146f7f5a3
Cam Spiers <cam.spiers@jnctn.nz>
2021-06-20 13:08:44 +1200

    Add request to filter function

--- fnl/snap/consumer/filter.fnl
+++ fnl/snap/consumer/filter.fnl
@@ -1,10 +1,9 @@
-(let [snap (require :snap)
-      tbl (snap.get :common.tbl)]
+(let [snap (require :snap)]
   (fn [filter-fnc producer]
     "Filters values from producer"
     (fn [request]
       (each [results (snap.consume producer request)]
         (if
           (and (= (type results) :table) (> (length results) 0))
-          (coroutine.yield (vim.tbl_filter filter-fnc results))
+          (coroutine.yield (vim.tbl_filter #(filter-fnc $1 request) results))
           (coroutine.yield results))))))
--- lua/snap/consumer/filter.lua
+++ lua/snap/consumer/filter.lua
@@ -1,11 +1,13 @@
 local _2afile_2a = "fnl/snap/consumer/filter.fnl"
 local snap = require("snap")
-local tbl = snap.get("common.tbl")
 local function _1_(filter_fnc, producer)
   local function _2_(request)
     for results in snap.consume(producer, request) do
       if ((type(results) == "table") and (#results > 0)) then
-        coroutine.yield(vim.tbl_filter(filter_fnc, results))
+        local function _3_(_241)
+          return filter_fnc(_241, request)
+        end
+        coroutine.yield(vim.tbl_filter(_3_, results))
       else
         coroutine.yield(results)
       end
