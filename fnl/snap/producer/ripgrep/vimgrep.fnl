168cb5848b67c8bb62750cea618f7d2383f5b3d2
Ahmed El Gabri <ahmed@gabri.me>
2021-06-21 21:50:13 +0200

    Add ripgrep.vimgrep.args

--- fnl/snap/producer/ripgrep/vimgrep.fnl
+++ fnl/snap/producer/ripgrep/vimgrep.fnl
@@ -1,5 +1,14 @@
 (let [snap (require :snap)
       general (snap.get :producer.ripgrep.general)]
-  (fn [request]
+  (local vimgrep {})
+  (local args [:--line-buffered :-M 100 :--vimgrep])
+  (fn vimgrep.default [request]
     (let [cwd (snap.sync vim.fn.getcwd)]
-      (general request {:args [:--line-buffered :-M 100 :--vimgrep request.filter] : cwd}))))
+      (general request {:args [(unpack args) request.filter] : cwd})))
+  (fn vimgrep.args [new-args cwd]
+    (let [args (tbl.concat args new-args)
+          absolute (not= cwd nil)]
+      (fn [request]
+        (let [cwd (or cwd (snap.sync vim.fn.getcwd))]
+          (general request {:args [(unpack args) request.filter] : cwd : absolute})))))
+  vimgrep)
--- lua/snap/producer/ripgrep/vimgrep.lua
+++ lua/snap/producer/ripgrep/vimgrep.lua
@@ -1,8 +1,19 @@
 local _2afile_2a = "fnl/snap/producer/ripgrep/vimgrep.fnl"
 local snap = require("snap")
 local general = snap.get("producer.ripgrep.general")
-local function _1_(request)
+local vimgrep = {}
+local args = {"--line-buffered", "-M", 100, "--vimgrep"}
+vimgrep.default = function(request)
   local cwd = snap.sync(vim.fn.getcwd)
-  return general(request, {args = {"--line-buffered", "-M", 100, "--vimgrep", request.filter}, cwd = cwd})
+  return general(request, {args = {unpack(args), request.filter}, cwd = cwd})
 end
-return _1_
+vimgrep.args = function(new_args, cwd)
+  local args0 = tbl.concat(args, new_args)
+  local absolute = (cwd ~= nil)
+  local function _1_(request)
+    local cwd0 = (cwd or snap.sync(vim.fn.getcwd))
+    return general(request, {absolute = absolute, args = {unpack(args0), request.filter}, cwd = cwd0})
+  end
+  return _1_
+end
+return vimgrep
