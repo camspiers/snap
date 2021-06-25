5b8d7f1b15aa0cb23e44d5dcaa27d72af22c2802
beauwilliams <beau.williams@icloud.com>
2021-06-25 10:08:18 +1000

    Adding changes to show untracked files with git file search

--- fnl/snap/producer/git/file.fnl
+++ fnl/snap/producer/git/file.fnl
@@ -2,4 +2,4 @@
       general (snap.get :producer.git.general)]
   (fn [request]
     (let [cwd (snap.sync vim.fn.getcwd)]
-      (general request {:args [:ls-files] : cwd}))))
+        (.. (general request {:args [:ls-files] : cwd}) (general request {:args [:ls-files :-o] : cwd})))))
--- lua/snap/producer/git/file.lua
+++ lua/snap/producer/git/file.lua
@@ -3,6 +3,6 @@ local snap = require("snap")
 local general = snap.get("producer.git.general")
 local function _1_(request)
   local cwd = snap.sync(vim.fn.getcwd)
-  return general(request, {args = {"ls-files"}, cwd = cwd})
+  return (general(request, {args = {"ls-files"}, cwd = cwd}) .. general(request, {args = {"ls-files", "-o"}, cwd = cwd}))
 end
 return _1_
