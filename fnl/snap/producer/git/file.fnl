b8796cdaee214c5da364ffc48815a87b28a2677f
beauwilliams <beau.williams@icloud.com>
2021-06-27 10:34:41 +1000

    fixing nesting of return statement

--- fnl/snap/producer/git/file.fnl
+++ fnl/snap/producer/git/file.fnl
@@ -3,6 +3,6 @@
   (fn [request]
     (let [cwd (snap.sync vim.fn.getcwd)]
         (if (not= (general request {:args [:ls-files :-o] : cwd}) nil)
-            (.. (general request {:args [:ls-files] : cwd}) (general request {:args [:ls-files :-o] : cwd}))))
-                (general request {:args [:ls-files] : cwd})))
+            (.. (general request {:args [:ls-files] : cwd}) (general request {:args [:ls-files :-o] : cwd})))
+                (general request {:args [:ls-files] : cwd}))))
 
--- lua/snap/producer/git/file.lua
+++ lua/snap/producer/git/file.lua
@@ -2,11 +2,9 @@ local _2afile_2a = "fnl/snap/producer/git/file.fnl"
 local snap = require("snap")
 local general = snap.get("producer.git.general")
 local function _1_(request)
-  do
-    local cwd = snap.sync(vim.fn.getcwd)
-    if (general(request, {args = {"ls-files", "-o"}, cwd = cwd}) ~= nil) then
-      do local _ = (general(request, {args = {"ls-files"}, cwd = cwd}) .. general(request, {args = {"ls-files", "-o"}, cwd = cwd})) end
-    end
+  local cwd = snap.sync(vim.fn.getcwd)
+  if (general(request, {args = {"ls-files", "-o"}, cwd = cwd}) ~= nil) then
+    do local _ = (general(request, {args = {"ls-files"}, cwd = cwd}) .. general(request, {args = {"ls-files", "-o"}, cwd = cwd})) end
   end
   return general(request, {args = {"ls-files"}, cwd = cwd})
 end
