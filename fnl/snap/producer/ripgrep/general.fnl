22c2b7d9c6021c74eec81ad737be2222cfefcb07
Milan Svoboda <msvoboda@ra.rockwell.com>
2024-07-03 19:01:30 +0200

    Do not start search until there is any input

--- fnl/snap/producer/ripgrep/general.fnl
+++ fnl/snap/producer/ripgrep/general.fnl
@@ -1,18 +1,21 @@
 (let [snap (require :snap)
-      io (snap.get :common.io)
-      string (snap.get :common.string)]
+           io (snap.get :common.io)
+           string (snap.get :common.string)]
   (fn [request {: args : cwd : absolute}]
-    (each [data err cancel (io.spawn :rg args cwd)]
-      (if
-        (request.canceled)
-        (do (cancel) (coroutine.yield nil))
-        (not= err "")
-        (coroutine.yield nil)
-        (= data "")
-        (snap.continue)
-        (do
-          (var results (string.split data))
+    (if
+      (= request.filter "")
+      (coroutine.yield nil)
+      (each [data err cancel (io.spawn :rg args cwd)]
+        (if
+          (request.canceled)
+          (do (cancel) (coroutine.yield nil))
+          (not= err "")
+          (coroutine.yield nil)
+          (= data "")
+          (snap.continue)
+          (do
+            (var results (string.split data))
 
-          (when absolute
-            (set results (vim.tbl_map #(string.format "%s/%s" cwd $1) results)))
-          (coroutine.yield results))))))
+            (when absolute
+              (set results (vim.tbl_map #(string.format "%s/%s" cwd $1) results)))
+            (coroutine.yield results)))))))
--- lua/snap/producer/ripgrep/general.lua
+++ lua/snap/producer/ripgrep/general.lua
@@ -7,26 +7,30 @@ local function _3_(request, _1_)
   local args = _arg_2_["args"]
   local cwd = _arg_2_["cwd"]
   local absolute = _arg_2_["absolute"]
-  for data, err, cancel in io.spawn("rg", args, cwd) do
-    if request.canceled() then
-      cancel()
-      coroutine.yield(nil)
-    elseif (err ~= "") then
-      coroutine.yield(nil)
-    elseif (data == "") then
-      snap.continue()
-    else
-      local results = string.split(data)
-      if absolute then
-        local function _4_(_241)
-          return string.format("%s/%s", cwd, _241)
-        end
-        results = vim.tbl_map(_4_, results)
+  if (request.filter == "") then
+    return coroutine.yield(nil)
+  else
+    for data, err, cancel in io.spawn("rg", args, cwd) do
+      if request.canceled() then
+        cancel()
+        coroutine.yield(nil)
+      elseif (err ~= "") then
+        coroutine.yield(nil)
+      elseif (data == "") then
+        snap.continue()
       else
+        local results = string.split(data)
+        if absolute then
+          local function _4_(_241)
+            return string.format("%s/%s", cwd, _241)
+          end
+          results = vim.tbl_map(_4_, results)
+        else
+        end
+        coroutine.yield(results)
       end
-      coroutine.yield(results)
     end
+    return nil
   end
-  return nil
 end
 return _3_
